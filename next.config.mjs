ab75d368b15e93e8998c4f0c736ec42acb75bc89
Bhavya Patel <bhavyapatel@Bhavyas-MacBook-Pro.local>
2025-09-18 00:15:21 -0400

    Configure for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/abc-research-v1>',
+  assetPrefix: '/abc-research-v1/',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
