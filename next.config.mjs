00b42a11595d158c4bf85b71eb9e04f544aefa8f
Bhavya Patel <bhavyapatel@Bhavyas-MacBook-Pro.local>
2025-09-18 09:49:47 -0400

    fix: Recreate next.config.mjs to resolve syntax issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
-THIS IS AN INTENTIONAL ERROR TO TEST THE BUILD;
+/**@type {import('next').NextConfig} */
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/abc-research-v1',
-  assetPrefix: '/abc-research-v1/',
-  images: {
-    unoptimized: true,
-  },
+    output: 'export',
+    basePath: '/abc-research-v1',
+    assetPrefix: '/abc-research-v1/',
+    images: {
+        unoptimized:true,
+    },
 };
 
 export default nextConfig;
